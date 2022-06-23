3c4c41fdca361b1e660bd36310d0291d35518a9c
Handrian Alandi <handrianalandi@gmail.com>
2022-06-23 19:35:35 +0700

    first push

---
+++ app.py
@@ -0,0 +1,109 @@
+import time
+from flask import Flask, jsonify, request, session
+from flask_session import Session
+from flask_mysqldb import MySQL
+from datetime import timedelta
+from uuid import uuid4
+from calculation import prime_index,palindrome_prime_index
+app = Flask(__name__)
+# session.clear()
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'simplecloud'
+mysql = MySQL(app)
+
+#flask session secret key
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SECRET_KEY'] = 'super secret key'
+app.config['SESSION_PERMANENT'] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+
+def set_session_timeout(minute):
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=minute)
+
+@app.route("/")
+def home():
+    # set_session_timeout(minute=1)
+    return "Hello, Flask!!asdsd"
+
+@app.route("/testing",methods=['POST'])
+def testing():
+    #get request body with key "name" json
+    name = request.json['name']
+    print(name)
+    return name
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if session.get('logged_in'):
+        return f"You are already logged in as {session['username']}!"
+    if request.method == 'GET':
+        return "Wrong method!"
+    if request.method == 'POST':
+        #get username form request body
+        username = request.json['username']
+        password = request.json['password']
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s", (username, password))
+        user = cursor.fetchone()
+        cursor.close()
+        if user:
+            #set secret key for session
+            session['secret_key'] = str(uuid4())
+            session['username'] = username
+            session['logged_in'] = True
+            set_session_timeout(minute=1)
+            return f"Welcome {username}"
+        else:
+            return "Invalid username or password"
+
+#logout
+@app.route('/logout', methods = ['POST'])
+def logout():
+    print("test")
+    if(session.get('username')):
+        session.clear()
+        return "Logged out"
+    return "You are not logged in"
+
+#register
+@app.route('/register', methods = ['POST'])
+def register():
+    if (session.get('username')):
+        return f"You are already logged in as {session['username']}!"
+    if request.method == 'POST':
+        #get username form request body
+        username = request.json['username']
+        password = request.json['password']
+        cursor = mysql.connection.cursor()
+        #register user to db
+        cursor.execute("INSERT INTO user (username, password) VALUES (%s, %s)", (username, password))
+        mysql.connection.commit()
+        cursor.close()
+        return "Registered, please login before using the app"
+
+@app.route('/api/prime/<n>', methods = ['GET'])
+def prime(n):
+    print("test")
+    if not session.get('logged_in'):
+        return "You are not logged in"
+    if request.method == 'GET':
+        #return jsonify result of prime_index function
+        
+        return jsonify({"result":prime_index(n)})
+
+# palindrome
+@app.route('/api/prime/palindrome/<n>', methods = ['GET'])
+def palindromeprimeindex(n):
+    if not session.get('logged_in'):
+        return "You are not logged in"
+    if request.method == 'GET':
+        palindrome_prime_index.delay(n)
+        # return jsonify({"result": palindrome_prime_index(n)})
+        return "Palindrom Prime Task has been called"
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
---
+++ calculation.py
@@ -0,0 +1,41 @@
+from celery import Celery
+import time
+
+app=Celery('calculation',backend='redis://127.0.0.1',broker='redis://127.0.0.1')
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def palindrome(n):
+    return str(n)==str(n)[::-1]
+
+@app.task()
+def prime_index(n):
+    counter=0
+    number=2
+    while counter!=int(n):
+        if is_prime(int(number)):
+            counter+=1
+        number+=1
+    return number-1
+
+
+
+
+@app.task()
+def palindrome_prime_index(n):
+    print("Palindrome prime function is called by celery worker..")
+    print("Asumption: 1 digit prime number is not considered as palindrome")
+    counter=0
+    number=10
+    time.sleep(5)
+    while counter!=int(n):
+        if is_prime(int(number)) and palindrome(int(number)):
+            counter+=1
+        number+=1
+    return number-1
